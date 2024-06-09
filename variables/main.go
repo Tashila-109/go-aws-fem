7f4f0b8187fc3352e15e20d4baa29732e10cbd7b
Tashila Fernando <tashila.dev@gmail.com>
2024-06-10 01:45:33 +0530

    task: variables

---
+++ variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	myName := "Tashila"
+	myInt := 10
+	myFloat := 10.0
+
+	fmt.Printf("Hello my name is %s and I am %d years old and my float is %f\n", myName, myInt, myFloat)
+}
